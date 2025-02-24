132437b91955a22dbc4e3eaf13b651601fd44fdf
Rajendra yadav <yadavrajendrayadav19999@gmail.com>
2025-02-24 16:44:54 +0530

    Time: 0 ms (100%), Space: 27.7 MB (6.24%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int left = 0;
-        int right = height.size() - 1;
-        int leftMax = height[left];
-        int rightMax = height[right];
-        int water = 0;
-
-        while (left < right) {
-            if (leftMax < rightMax) {
-                left++;
-                leftMax = max(leftMax, height[left]);
-                water += leftMax - height[left];
-            } else {
-                right--;
-                rightMax = max(rightMax, height[right]);
-                water += rightMax - height[right];
-            }
+    int trap(vector<int>& a) {
+        int n = a.size();
+        vector<int>pge(n);
+        vector<int>nge(n);
+        vector<int>c(n);
+        pge[0]=-1;
+        nge[n-1]=-1;
+        int maxi=a[0];
+        for(int i=1;i<n;i++){
+            pge[i]=maxi;
+           maxi= max(maxi,a[i]);
         }
+        maxi=a[n-1];
+        for(int i=n-2;i>=0;i--){
+            nge[i]=maxi;
+            maxi=max(maxi,a[i]);
+        }
+        int sum=0;
+        for(int i=1;i<n-1;i++){
+            c[i]=min(pge[i],nge[i]);
+            if(a[i]<c[i]){
+                sum+=(c[i]-a[i]);
+            }
 
-        return water;        
+        }
+        return sum;
+
     }
 };
