2cb4f2252e86c37f71c03ccb8575d00fe48bb359
Rajendra yadav <yadavrajendrayadav19999@gmail.com>
2025-03-13 17:50:58 +0530

    Time: 0 ms (100%), Space: 14 MB (60.87%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& Arr) {
+        long long int x = 0;
+        int bucket1 = 0, bucket2 = 0;
+        long long int N=Arr.size();
+        
+        for (long long int i = 0; i < N; i++) {
+            x ^= Arr[i];
+        }
+    
+        long long int num =x & -x;
+        
+        
+    
+        for ( long long int i = 0; i < N; i++) {
+            if (Arr[i] & num) bucket1 ^= Arr[i];
+            else bucket2 ^= Arr[i];
+        }
+    
+        if (bucket1 < bucket2) swap(bucket1, bucket2);
+        
+        
+        return {bucket1, bucket2};
+    }
+};
