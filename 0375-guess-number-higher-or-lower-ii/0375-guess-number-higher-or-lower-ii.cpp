0cc7303839056d06cc5b40063acf89373a2b5789
Rajendra yadav <yadavrajendrayadav19999@gmail.com>
2025-08-22 17:21:04 +0530

    Time: 47 ms (58.43%), Space: 11.2 MB (23.83%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    int memoization(int start, int end, vector<vector<int>>& dp) {
-        if (start >= end)
-            return 0;
-        if(dp[start][end]!=-1)return dp[start][end];
-        int ans = INT_MAX;
-        for (int i = start; i <= end; i++) {
-            ans = min(ans,
-                      i + max(memoization(start, i - 1,dp), memoization(i + 1, end,dp)));
+    int tabulation(int n) {
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+
+        for (int start = n - 1; start >= 0; start--) {
+            for (int end = 1; end <= n; end++) {
+                if (start == end) {
+                    dp[start][end] = 0;
+                } else if (start < end) {
+                    int ans = INT_MAX;
+                    for (int i = start; i <= end; i++) {
+                        int left = (i - 1 >= start) ? dp[start][i - 1] : 0;
+                        int right = (i + 1 <= end) ? dp[i + 1][end] : 0;
+                        ans = min(ans, i + max(left, right));
+                    }
+                    dp[start][end] = ans;
+                }
+            }
         }
-        return dp[start][end]=ans;
+
+        return dp[1][n];
     }
-    int recursion(int start, int end) {
-        if (start >= end)
-            return 0;
-        int ans = INT_MAX;
-        for (int i = start; i <= end; i++) {
-            ans = min(ans,
-                      i + max(recursion(start, i - 1), recursion(i + 1, end)));
-        }
-        return ans;
-    }
-    int getMoneyAmount(int n) {
-        // return recursion(1,n);
-        vector<vector<int>> dp(
-            n + 1, vector<int>(n + 1, -1));
-             return memoization(1, n, dp);
-    }
+
+    int getMoneyAmount(int n) { return tabulation(n); }
 };
