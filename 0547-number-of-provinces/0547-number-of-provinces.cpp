5109badaa11ae8234188baa59e257178c0e64b41
Rajendra yadav <yadavrajendrayadav19999@gmail.com>
2025-08-31 11:27:08 +0530

    Time: 0 ms (100%), Space: 20.7 MB (38.01%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void bfs(int src,vector<bool>&visited,vector<vector<int>>&adj){
+        queue<int>q;
+        q.push(src);
+        visited[src]=true;
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            for(auto n:adj[u]){
+                if(!visited[n]){
+                    q.push(n);
+                    visited[n]=true;
+                }
+            }
+        }
+
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        vector<vector<int>>adj(n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j && isConnected[i][j]==1){
+                adj[i].push_back(j);}}
+
+            }
+        
+        vector<bool>visited(n,false);
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                count++;
+                bfs(i,visited,adj);
+            }
+        }
+        return count;
+
+    }
+};
