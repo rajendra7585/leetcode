b4c5b8ebfeb9ac8e529b87bf8dbf524c5ab06614
Rajendra yadav <yadavrajendrayadav19999@gmail.com>
2025-12-05 22:03:25 +0530

    Time: 179 ms (18.47%), Space: 207.6 MB (6.09%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+   bool tabul(int target,vector<int>&nums){
+    int n=nums.size();
+    vector<vector<int>>dp(n+1,vector<int>(target+1,0));
+    for(int i=0;i<nums.size();i++){
+        dp[i][0]=1;
+    }
+    for(int index=n-1;index>=0;index--){
+        for(int t=1;t<=target;t++){
+            bool include=0;
+            if(t-nums[index]>=0)include=dp[index+1][t-nums[index]];
+            int exclude=dp[index+1][t];
+            dp[index][t]=(include||exclude);
+        }
+    }
+    return dp[0][target];
+   }
    
     bool recurs(int i,int target,vector<int>&nums,vector<vector<int>>&dp){
         int n=nums.size();
@@ -21,7 +37,7 @@ public:
         if(sum&1)return false;
         int target=sum/2;
         vector<vector<int>>dp(n,vector<int>(target+1,-1));
-        bool ans=recurs(0,target,nums,dp);
+        bool ans=tabul(target,nums);
         return ans;
     }
 };
