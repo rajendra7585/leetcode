c7a33fe5e5a8b5e639f59f185e60fb68cfd4b324
Rajendra yadav <yadavrajendrayadav19999@gmail.com>
2025-12-05 21:54:20 +0530

    Time: 172 ms (21.49%), Space: 110.4 MB (39.07%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,66 +1,27 @@
 class Solution {
 public:
-    int tabulation(vector<int>& nums, int n, int s, int i
-                    ) {
-        vector<vector<int>>dp(n+1,vector<int>(s+1,0));
-        for(int i=0;i<n;i++){
-            dp[i][0]=1;
+   
+    bool recurs(int i,int target,vector<int>&nums,vector<vector<int>>&dp){
+        int n=nums.size();
+        if(i>=n)return 0;
+        if(target<0)return 0;
+        if(target==0)return 1;
+        if(dp[i][target]!=-1)return dp[i][target];
+        bool include=recurs(i+1,target-nums[i],nums,dp);
+        bool exclude=recurs(i+1,target,nums,dp);
+        return dp[i][target]=(include||exclude);
+    }
+    
+    bool canPartition(vector<int>& nums) {
+        int sum=0;
+        int n=nums.size();
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
         }
-        for(int i=n-1;i>=0;i--){
-            for(int j=1;j<=s;j++){
-        int include = 0;
-        if (j-nums[i]>=0) {
-            include = dp[i+1][j-nums[i]];
-        }
-        int exclude = dp[i+1][j];
-        dp[i][j]=(include||exclude);}}
-        return dp[0][s];
+        if(sum&1)return false;
+        int target=sum/2;
+        vector<vector<int>>dp(n,vector<int>(target+1,-1));
+        bool ans=recurs(0,target,nums,dp);
+        return ans;
     }
-
-int memoization(vector<int>& nums, int n, int s, int i,
-                  vector<vector<int>>& dp) {
-    if (s < 0)
-        return 0;
-    if (i >= n)
-        return 0;
-    if (s == 0)
-        return 1;
-    if (dp[i][s] != -1)
-        return dp[i][s];
-    int include = 0;
-    if (nums[i] <= s) {
-        include = memoization(nums, n, s - nums[i], i + 1, dp);
-    }
-    int exclude = memoization(nums, n, s, i + 1, dp);
-    return dp[i][s] = (include || exclude);
-}
-// }  int srecursion(vector<int>& nums, int n, int s, int i) {
-//     if (s < 0)
-//         return 0;
-//     if (i >= n)
-//         return 0;
-//     if (s ==
-//         return 1;
-//     int include = 0;
-//     if (nums[i] <= s) {
-//         include = srecursion(nums, n, s - nums[i], i + 1);
-// }
-//     int exclude = srecursion(nums, n, s, i + 1);
-//     return include || exclude;
-// }
-bool canPartition(vector<int>& nums) {
-    int n = nums.size();
-    int sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += nums[i];
-    }
-    if (sum & 1)
-        return 0;
-    int s = sum / 2;
-    int i = 0;
-    // return srecursion(nums,n,s,i);
-    //     vector<vector<int>> dp(n + 1, vector<int>(s+1, -1));
-    //     return memoization(nums,n, s, i, dp);
-    // }
-    return tabulation(nums, n, s, i);
-}};
+};
