974a6e15abd0e31f36fbeae76d2b0544df848550
Rajendra yadav <yadavrajendrayadav19999@gmail.com>
2025-12-12 22:41:19 +0530

    Time: 7 ms (42.99%), Space: 24.9 MB (14.79%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& a) {
-        sort(a.begin(),a.end(),[](const auto &a,const auto &b){
-            return a[0]<b[0];
+        sort(a.begin(),a.end(),[](const vector<int>&b,const vector<int>&c){
+            if(b[0]!=c[0])return b[0]<c[0];
+            else return c[0]<b[0];
         });
         int n=a.size();
-        vector<vector<int> > ans;
+        vector<vector<int>>ans;
         ans.push_back(a[0]);
         for(int i=1;i<n;i++){
             vector<int>curr=a[i];
-            if(curr[0]<=ans[ans.size()-1][1]){
-        ans[ans.size()-1][0]=min(ans[ans.size()-1][0],curr[0]);
+            if(ans[ans.size()-1][1]>=curr[0]){
+                ans[ans.size()-1][0]=min(ans[ans.size()-1][0],curr[0]);
                 ans[ans.size()-1][1]=max(ans[ans.size()-1][1],curr[1]);
-
             }
-            else{
-                ans.push_back(a[i]);
-            }
+            else ans.push_back(curr);
         }
-        return ans;
+
+       return ans;
+        
     }
 };
