e7ec5915ea00e57b9a3e4cbb4ddd04f73f53db60
Rajendra yadav <yadavrajendrayadav19999@gmail.com>
2025-12-12 23:37:58 +0530

    Time: 0 ms (100%), Space: 8.6 MB (26.93%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        unordered_map<char, int> f;
-        int n = s.length();
-        for (int i = 0; i < n; i++) {
+        unordered_map<char,int>f;
+        int n=s.length();
+        for(int i=0;i<n;i++){
             f[s[i]]++;
         }
-        char prev = '#';
-        string ans = "";
-        for (int k = 0; k < n; k++) {
-            char best = 0;
-            int bc = -1;
-            for (const auto j : f) {
-                if (j.second > 0 && j.first != prev) {
-                    if (j.second > bc) {
-                        best = j.first;
-                        bc = j.second;
+        char prev='#';
+        string ans="";
+        for(int k=0;k<n;k++){
+            char best=0;
+            int bc=-1;
+            for(const auto i:f){
+                if(i.second>0 && i.first!=prev){
+                    if(i.second>bc){
+                        best=i.first;
+                        bc=i.second;
                     }
                 }
             }
-            if (bc == -1)
-                return "";
-
+            if(bc==-1)return "";
             ans.push_back(best);
             f[best]--;
-            prev = best;
+            prev=best;
         }
         return ans;
     }
