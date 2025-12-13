9b9a65222c3c59f48f36407f2099db54c69fa231
Rajendra yadav <yadavrajendrayadav19999@gmail.com>
2025-12-13 11:33:16 +0530

    Time: 0 ms (100%), Space: 20.5 MB (35.07%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n=nums.size();
-        int last=0;
         int curr=0;
+        int far=0;
         int jumps=0;
-        if(n<=1)return jumps;
-        for(int i=0;i<n;i++){
-            last=max(last,i+nums[i]);
-            if(curr==i){
-                jumps++;
-                curr=last;
+        for(int i=0;i<n-1;i++){
+            far=max(far,i+nums[i]);
+            if(i==curr){
+               jumps++;
+               curr=far;
             }
-            if(curr>=n-1)break;
         }
         return jumps;
     }
