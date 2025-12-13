1091bfc1c5b7485993de954c63daffb4ff801fa6
Rajendra yadav <yadavrajendrayadav19999@gmail.com>
2025-12-13 12:02:33 +0530

    Time: 35 ms (48.01%), Space: 52.8 MB (16.29%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int tabl(int n, vector<int>& coins, int amt) {
-        vector<int> dp(amt + 1, 1e9);
-        dp[0] = 0;
-        for (int i = 1; i <= amt; i++)
-            for (int c : coins)
-                if (i >= c)
-                    dp[i] = min(dp[i], dp[i - c] + 1);
-
-        return dp[amt] == 1e9 ? -1 : dp[amt];
+int tabl(int n,vector<int>& coins,int amount){
+    vector<int>dp(amount+1,INT_MAX);
+    dp[0]=0;
+    for(int i=1;i<=amount;i++){
+        for(int c:coins){
+            if(i>=c && dp[i-c]!=INT_MAX){
+            dp[i]=min(dp[i],1+dp[i-c]);}
+        }
     }
-    int recurs(vector<int>& coins, int idx, int amount,
-               vector<vector<int>>& dp) {
+    return dp[amount];
+}
+    int recurs(vector<int>& coins, int idx, int amount,vector<vector<int>>&dp) {
         if (amount == 0)
             return 0;
         if (idx == 0) {
@@ -20,24 +20,22 @@ public:
             else
                 return INT_MAX;
         }
-        if (dp[idx][amount] != -1)
-            return dp[idx][amount];
-
+        if(dp[idx][amount]!=-1)return dp[idx][amount];
+        
         int include = INT_MAX;
-        if (amount >= coins[idx]) {
-            int sub = recurs(coins, idx, amount - coins[idx], dp);
-            if (sub != INT_MAX) {
-                include = 1 + sub;
-            }
-        }
-        int exclude = recurs(coins, idx - 1, amount, dp);
-        return dp[idx][amount] = min(include, exclude);
+        if (amount >= coins[idx]){
+            int sub=recurs(coins, idx , amount - coins[idx],dp);
+            if(sub!=INT_MAX){
+                include=1+sub;
+            }}
+            int exclude = recurs(coins, idx - 1, amount,dp);
+        return dp[idx][amount]=min(include, exclude);
     }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-        int ans = tabl(n, coins, amount);
+        
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int ans = tabl(n,coins,amount);
         if (ans == INT_MAX)
             return -1;
         return ans;
