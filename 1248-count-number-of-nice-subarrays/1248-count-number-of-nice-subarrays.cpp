e46b1901fd47a36edd82c5a44fe26737c96a92ce
Rajendra yadav <yadavrajendrayadav19999@gmail.com>
2026-01-09 16:39:52 +0530

    Time: 4 ms (83.72%), Space: 71.4 MB (70.85%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    int rajendra(vector<int>& nums, int k) {
-        int len = nums.size(), r = 0, l = 0, sum = 0, count = 0;
-        int countOdd=0;
-        while (r < len) {
-            countOdd += nums[r] % 2;
-            while (countOdd > k) {
-                if (nums[l] % 2 != 0)
-                    countOdd--;
-                l++;
-            }
-            count += (r - l + 1);
-            r++;
-        }
-        return count;
-    }
-
-  int numberOfSubarrays(vector<int>& nums, int k) {
+   int solve(vector<int>&nums,int k){
+    int l=0;
     int n=nums.size();
-    return rajendra(nums,k)-rajendra(nums,k-1);
-}
-}
-;
+    int r=0;
+    int count=0;
+    int oddcount=0;
+    while(r<n){
+        oddcount+=nums[r]%2;
+        while(oddcount>k){
+            if(nums[l]%2)oddcount--;
+            l++;
+        }
+        count+=(r-l+1);
+        r++;
+
+    }
+    return count;
+
+   }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int n=nums.size();
+        return solve(nums,k)-solve(nums,k-1);
+
+    }
+};
